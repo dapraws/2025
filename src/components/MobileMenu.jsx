6f10b8fb10c6000282f54fd526288c00bd5a0b8b
dapraws <darrelprawira26@gmail.com>
2025-06-02 10:14:24 +0700

    feat: navigation bar

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -46,7 +46,18 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
         About
       </a>
       <a
-        href="#projects"
+        href="#experience"
+        onClick={() => setMenuOpen(false)}
+        className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+            ${
+              menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
+            }        
+    `}
+      >
+        Experience
+      </a>
+      <a
+        href="#Projects"
         onClick={() => setMenuOpen(false)}
         className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
             ${
@@ -55,17 +66,6 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
     `}
       >
         Projects
-      </a>
-      <a
-        href="#contact"
-        onClick={() => setMenuOpen(false)}
-        className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
-            ${
-              menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
-            }        
-    `}
-      >
-        Contact
       </a>
     </div>
   );
