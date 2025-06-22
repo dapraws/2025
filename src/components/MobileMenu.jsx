387bbd9657dc1e9306ba47a2eebae0c56493cce1
dapraws <darrelprawira26@gmail.com>
2025-06-23 02:07:54 +0700

    fix: mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -34,6 +34,7 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
       >
         Home
       </a>
+
       <a
         href="#about"
         onClick={() => setMenuOpen(false)}
@@ -44,18 +45,20 @@ export const MobileMenu = ({ menuOpen, setMenuOpen }) => {
     `}
       >
         About
-        <a
-          href="#projects"
-          onClick={() => setMenuOpen(false)}
-          className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
+      </a>
+
+      <a
+        href="#projects"
+        onClick={() => setMenuOpen(false)}
+        className={`text-2xl font-semibold text-white my-4 transform transition-transform duration-300
             ${
               menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-5"
             }        
     `}
-        >
-          Projects
-        </a>
+      >
+        Projects
       </a>
+
       <a
         href="#contact"
         onClick={() => setMenuOpen(false)}
